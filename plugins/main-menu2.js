d1555ae40376429fa66aab439b5dbb7afe6a0959
irsyad-MD <114546602+irsyad-MD@users.noreply.github.com>
2022-10-24 10:34:30 +0700

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -8,7 +8,7 @@ let suka = `Saya irsyad`
 global.fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': suka, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${suka},;;;\nFN:${suka},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync('./thumbnail.jpg'), thumbnail: fs.readFileSync('./thumbnail.jpg'),sendEphemeral: true}}}
 
 let str = `*Hai Ngab*`
-let isi =`I'm 𝖔𝖋𝖈.𝚂𝚢𝚊𝚍𝘣𝘰𝘵𝘻𝘝3 Yang Di Buat Gak Niat Oleh irsyad Karena Males Recode Jadi Pake Aja Apa Yang Ada Yah.\nKalau Ada Yg Error Bisa Langsung Report Ke Owner.\n${tag} (Owner)`
+let isi =`I'm 𝖔𝖋𝖈.𝚂𝚢𝚊𝚍𝘣𝘰𝘵𝘻𝘝2 Yang Di Buat Gak Niat Oleh irsyad Karena Males Recode Jadi Pake Aja Apa Yang Ada Yah.\nKalau Ada Yg Error Bisa Langsung Report Ke Owner.\n${tag} (Owner)`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.send3ButtonImg(m.chat, `https://telegra.ph/file/f6271f872573b6524b1e5.jpg`, str, isi, 'Menu', '.command', 'Owner', '.owner', 'Credit', '.credit', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
