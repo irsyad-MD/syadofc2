8b586700afe12adb03c39e5eb28af22390b569a0
irsyad-MD <irsyadtillah1@gmail.com>
2022-10-24 10:13:35 +0700

    babayo

---
+++ plugins/info-script.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch')
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*-
+sc ini: ga ada
+buy bang ga grtis komtil🥲`
+let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'O W N E R','.owner', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/baukpandan_1324",
+    mediaType: "VIDEO",
+    description: "kamu celek", 
+    title: 'ofc.syadBotz',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: urlnya
+  }
+  } }) 
+          }
+handler.help = ['script']
+handler.tags = ['info']
+handler.command =  /^(script|sc)$/i
+
+module.exports = handler
